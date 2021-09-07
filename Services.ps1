b6bbebfb035186b87dac23da909f6e118331b767
blevell <90269848+blevell@users.noreply.github.com>
2021-09-07 15:36:05 -0700

    Add files via upload

---
+++ Services.ps1
@@ -0,0 +1,13 @@
+﻿$Computername = Read-Host 'Enter a computer name'
+$services = Get-Content C:\services.txt
+
+
+ForEach ($s in $services) {
+    $starttype = (Get-Service -ComputerName $Computername -Name $s).StartType
+    If ($Starttype -ne "Automatic") {
+        Set-Service -ComputerName $Computername $s -StartupType Automatic
+        Write-Host "Startup set to automatic for $s"
+    } Else {
+        Write-Host "$s is already started automatically"
+    }
+}
